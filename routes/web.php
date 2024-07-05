a82c5cb2b63e32e04ccce5f276b2d8becb43aaa3
Rakesh Tolangi <tolangirakesh67@gmail.com>
2024-07-05 18:28:19 +0545

    product route setup
    setting up route, model, controller and database for product

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProductController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/product', [ProductController::class, 'index'])->name('product.index');
