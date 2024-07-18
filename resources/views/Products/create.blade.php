e37cd4e8a74d2fa4c8173c6c7a378058e9a68af1
Rakesh Tolangi <tolangirakesh67@gmail.com>
2024-07-18 09:20:55 +0545

    adding new interface

--- resources/views/Products/create.blade.php
+++ resources/views/Products/create.blade.php
@@ -4,11 +4,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     <title>Create product</title>
 </head>
 
 <body>
+
+    <!--Nav bar section  -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/">sasto bazzar </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+
+                <li class="nav-item">
+                    <a class="nav-link" href="product/create">Add new product</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="product/">View products</a>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
+
+
     <h1> create the product</h1>
+
     @if ($message = Session::get('success'))
     <div class="alert alert-success">
         <p>{{ $message }}</p>
@@ -16,31 +44,41 @@
     @endif
     <form action="{{route('product.store')}}" method="post">
         @csrf
-        <div>
+        <div class="mb-3 mt-3">
             <label for="">Name</label>
-            <input type="text" name="name" placeholder="name" />
+            <input type=" text" name="name" placeholder="name" />
         </div>
 
-        <div>
+        <div class="mb-3">
             <label for="">Quatity</label>
             <input type="qty" name="qty" placeholder="qty" />
         </div>
 
-        <div>
+        <div class="mb-3">
             <label for="">Price</label>
             <input type="text" name="price" placeholder="price" />
         </div>
 
-        <div>
+        <div class="mb-3">
             <label for="">Description</label>
             <input type="text" name="description" placeholder="Description" />
         </div>
 
-        <div>
-            <input type="submit" value="Save a New Product" />
+        <div class="mb-3">
+            <input type="submit" value="Save a New Product" class="btn btn-primary" />
         </div>
     </form>
+    <!-- view product button -->
+    <div>
+        <a href="/product" class="btn btn-primary stretched-link"> View all products</a>
 
+    </div>
+
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
--- resources/views/Products/index.blade.php
+++ resources/views/Products/index.blade.php
@@ -4,13 +4,68 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     <title>products</title>
 </head>
 
 <body>
+
+    <style>
+    body {
+        padding: 15px;
+    }
+
+    table,
+    th,
+    td {
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+
+
+    th,
+    td {
+        padding: 5px;
+    }
+
+    tr:hover {
+        background-color: #D6EEEE;
+    }
+
+    tr {
+        border-bottom: 1px solid #ddd;
+    }
+    </style>
+
+    <!--Nav bar section  -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/">sasto bazzar </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+
+                <li class="nav-item">
+                    <a class="nav-link" href="product/create">Add new product</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="product/">View products</a>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
+
     <h1>products</h1>
+
     <p>lists of product</p>
-    <table>
+
+    <table boarder="1">
         <thead>
             <th>
                 <tr>S.N</tr>
@@ -33,6 +88,18 @@
             @endforeach
         </tbody>
     </table>
+
+    <div>
+        <a href="http://127.0.0.1:8000/product/create" class="btn btn-primary stretched-link">Add new product</a>
+
+    </div>
+
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
 </body>
 
 </html>
